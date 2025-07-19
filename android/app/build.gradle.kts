20e2313f28a8aec7ec484a7183a10b2ad2e82dea
Harsh Sagar Bothara <harshbothara24@gmail.com>
2025-07-19 15:09:21 +0530

    fixed google signin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,21 +1,14 @@
-
-
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
-    
 }
 
 android {
-    namespace = "com.beeman.beeman"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    namespace = "com.example.beeman"
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -28,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.beeman.beeman"
+        applicationId = "com.example.beeman"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/beeman/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/beeman/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.beeman.beeman
+package com.example.beeman
 
 import io.flutter.embedding.android.FlutterActivity
 
